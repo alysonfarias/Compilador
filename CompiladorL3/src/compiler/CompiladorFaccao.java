2e2bb7e6562ffcfba0d0412c109c23904bc178a5
Alyson <ramos.alysonfarias@gmail.com>
2022-04-20 22:38:52 -0300

    adicionado operacao de char e consertado o erro do arquivo

--- CompiladorL3/src/compiler/CompiladorFaccao.java
+++ CompiladorL3/src/compiler/CompiladorFaccao.java
@@ -1,14 +1,12 @@
 package compiler;
 
 public class CompiladorFaccao {
-	public static void main(String[] args) {
 
-		Lexico lexico = new Lexico("CompiladorL3\\src\\compiler\\codigo.txt");
-		Token t = null;
-		while ((t = lexico.nextToken()) != null) {
-			System.out.println(t.toString());
-		}
-
-	}
-
+    public static void main(String[] args) {
+        Lexico lexico = new Lexico("src\\compiler\\codigo.txt");
+        Token t = null;
+        while ((t = lexico.nextToken()) != null) {
+            System.out.println(t.toString());
+        }
+    }
 }
--- CompiladorL3/src/compiler/Token.java
+++ CompiladorL3/src/compiler/Token.java
@@ -1,61 +1,65 @@
 package compiler;
 
 public class Token {
-	public static int IDENTIFIER_TYPE = 1;
-	public static int INTEGER_TYPE = 2;
-	public static int REAL_TYPE = 3;
-	public static int CHAR_TYPE = 4;
-	public static int RELACIONAL_OPERATOR = 5;
-	public static int ARITHMETIC_OPERATOR = 6;
-	public static int SPECIAL_CHARACTER = 7;
-	public static int ASSIGNMENT_TYPE = 8;
-	public static int INLINE_COMMENT = 9;
-	public static int TIPO_RESERVED_WORD = 10;
-	public static int ENDCODE_TYPE = 99;
 
-	private int type;
-	private String lexeme;
+    public static int IDENTIFIER_TYPE = 1;
+    public static int INTEGER_TYPE = 2;
+    public static int REAL_TYPE = 3;
+    public static int CHAR_TYPE = 4;
+    public static int RELACIONAL_OPERATOR = 5;
+    public static int ARITHMETIC_OPERATOR = 6;
+    public static int SPECIAL_CHARACTER = 7;
+    public static int ASSIGNMENT_TYPE = 8;
+    public static int INLINE_COMMENT = 9;
+    public static int TIPO_RESERVED_WORD = 10;
+    public static int DE_IN_CREMENT = 11;
+    public static int ENDCODE_TYPE = 99;
 
-	public Token(String lexeme, int tipo) {
-		this.lexeme = lexeme;
-		this.type = tipo;
-	}
+    private int type;
+    private String lexeme;
 
-	public String getLexeme() {
-		return this.lexeme;
-	}
+    public Token(String lexeme, int tipo) {
+        this.lexeme = lexeme;
+        this.type = tipo;
+    }
 
-	public int getType() {
-		return this.type;
-	}
+    public String getLexeme() {
+        return this.lexeme;
+    }
 
-	@Override
-	public String toString() {
-		switch (type) {
-		case 1:
-			return this.lexeme + " - IDENTIFIER";
-		case 2:
-			return this.lexeme + " - INTEGER";
-		case 3:
-			return this.lexeme + " -REAL";
-		case 4:
-			return this.lexeme + " - CHAR";
-		case 5:
-			return this.lexeme + " - RELACIONAL_OPERATOR";
-		case 6:
-			return this.lexeme + " - ARITHMETIC_OPERATOR";
-		case 7:
-			return this.lexeme + " - SPECIAL_CHARACTER";
-		case 8:
-			return this.lexeme + " - ASSIGNMENT";
-		case 9:
-			return this.lexeme + " - INLINE_COMMENT";
-		case 10:
-			return this.lexeme + " - RESERVED_WORD";
-		case 99:
-			return this.lexeme + " - END_CODE";
-		}
-		return "";
-	}
+    public int getType() {
+        return this.type;
+    }
+
+    @Override
+    public String toString() {
+        switch (type) {
+            case 1:
+                return this.lexeme + " - IDENTIFIER";
+            case 2:
+                return this.lexeme + " - INTEGER";
+            case 3:
+                return this.lexeme + " - REAL";
+            case 4:
+                return this.lexeme + " - CHAR";
+            case 5:
+                return this.lexeme + " - RELACIONAL_OPERATOR";
+            case 6:
+                return this.lexeme + " - ARITHMETIC_OPERATOR";
+            case 7:
+                return this.lexeme + " - SPECIAL_CHARACTER";
+            case 8:
+                return this.lexeme + " - ASSIGNMENT";
+            case 9:
+                return this.lexeme + " - INLINE_COMMENT";
+            case 10:
+                return this.lexeme + " - RESERVED_WORD";
+            case 11:
+                return this.lexeme + " - RESERVED_WORD";
+            case 99:
+                return this.lexeme + " - END_CODE";
+        }
+        return "";
+    }
 
 }
